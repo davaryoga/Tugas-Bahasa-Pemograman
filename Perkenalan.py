2e20982cfc440cac7df2141d01f64c3738f2a54d
davaryoga <92939686+davaryoga@users.noreply.github.com>
2021-10-24 17:13:50 +0700

    Add files via upload

---
+++ Perkenalan.py
@@ -0,0 +1,5 @@
+print("Asalam`ualikum")
+print(" ")
+print("Let me Introduce my self. My name is Muhammad Dava Aryoga, but you can call me Dava. My NPM is 312110552. I was born in Jakarta and i am 21 years old. i am very glad if you want to invite my house in Bendungan Melayu Street, No. 20, Koja, Jakarta Utara, 14230. So don`t forget to call e before with the number 085155477417.")
+print(" ")
+print("Thank You")
