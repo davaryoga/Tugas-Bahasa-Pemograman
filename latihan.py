c802f495190766feb9a9615f7ce10676faf6f787
davaryoga <92939686+davaryoga@users.noreply.github.com>
2021-10-26 17:43:03 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -13,3 +13,6 @@ penjelasan program:
      i am very glad if you want to invite my house in Bendungan Melayu Street, No. 20, Koja, Jakarta Utara, 14230. 
      So don`t forget to call e before with the number 085155477417.
   3. perintah print("Thank you") akan menghasilkan output Thank You  
+
+berikut adalah screenshoot outuput dari program di atas 
+![Screenshot (28)](https://user-images.githubusercontent.com/92939686/138862119-fb8fa90e-ab19-4930-925a-8471bb75293e.png)                   
