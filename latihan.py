41d2c646a9b0dc5b227d75f88b1a906880151a8e
davaryoga <92939686+davaryoga@users.noreply.github.com>
2021-10-26 17:40:12 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -3,3 +3,13 @@ print(" ")
 print("Let me Introduce my self. My name is Muhammad Dava Aryoga, but you can call me Dava. My NPM is 312110552. I was born in Jakarta and i am 21 years old. i am very glad if you want to invite my house in Bendungan Melayu Street, No. 20, Koja, Jakarta Utara, 14230. So don`t forget to call e before with the number 085155477417.")
 print(" ")
 print("Thank You")
+
+penjelasan program:
+  1. perintah print("Assalamu`alaikum) akan menghasilkan output Assalamualikum
+  2. perintah print("Let me Introduce my self. My name is Muhammad Dava Aryoga, but you can call me Dava. My NPM is 312110552. I was born in Jakarta and i am 21 years old. i am very glad if you want to invite my house in Bendungan Melayu Street, No. 20, Koja, Jakarta Utara, 14230. So don`t forget to call e before with the number 085155477417.")
+     akan menghhasilkan output 
+     Let me Introduce my self. My name is Muhammad Dava Aryoga, but you can call me Dava.
+     My NPM is 312110552. I was born in Jakarta and i am 21 years old. 
+     i am very glad if you want to invite my house in Bendungan Melayu Street, No. 20, Koja, Jakarta Utara, 14230. 
+     So don`t forget to call e before with the number 085155477417.
+  3. perintah print("Thank you") akan menghasilkan output Thank You  
